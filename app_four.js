eed3f5fd375479cb630b933fb80db551adf90578
Bunks18 <wbunker11@gmail.com>
2017-03-13 16:36:06 -0400

    changes

--- app_four.js
+++ app_four.js
@@ -7,6 +7,9 @@ var client_secret = 'M1TVJLFZC1HKUNAUXOF1FW3W1IIP1T0RTEPEYGTSKV50OGNM';
 var v= '20170301';
 var map = null;
 
+var globLat = '';
+var globLong = '';
+
 //initialize  the state
 state = {
   searches: [],
@@ -33,6 +36,7 @@ function getLocation() {
     } else {
         console.log("Geolocation is not supported by this browser.")
     }
+
 }
 
 //get location
@@ -41,6 +45,8 @@ function showPosition(position) {
     console.log(latlon);
     lat = position.coords.latitude;
     long = position.coords.longitude;
+    globLat = position.coords.latitude;
+    globLong = position.coords.longitude;
   }
 
 // add markers to geojson object
@@ -65,54 +71,41 @@ function showPosition(position) {
   }
 
 
-
-
 //create map
-  function createMap(lat,long, geojson){
-    map = L.mapbox.map('map-one', 'mapbox.streets').setView([lat, long], 14);
+  function createMap(globLat,globLong, geojson){
+    map = L.mapbox.map('map-one', 'mapbox.streets').setView([globLat, globLong], 14);
     L.mapbox.accessToken = token;
     var myLayer = L.mapbox.featureLayer().addTo(map);
     myLayer.setGeoJSON(geojson);
-//
-// var newArray = geojson.features.filter(function (el) {
-//     return el.geojson.features.properties.query === query;
-//   });
-
-// function filter(geojson,query) {
-//
-//   var newArray = {
-//         type: 'FeatureCollection',
-//         features: []
-//       };
-//
-// for (var i = 0; i < geojson.features.length ; i++) {
-//     if (geojson.features[i].properties.query === query) {
-//         newArray.push(geojson.features[i]);
-//     }
-// }
-//
-// }
-
-
-
   }
 
-
-  //write previous results
-
+//write previous results
   function writeResults(state){
     for (var i=0; i<state.searches.length; i++) {
           var html= '';
-          html+= '<a class="searchLink">'+ state.searches[i].search +'</a> <br>';
+          html = '<a class="searchLink">'+ state.searches[i].search +'</a> <br>';
 
       }
         $('#prev-search').append(html);
     }
 
+
+// Previous search function
+function getPreviousSearch(state, searchText) {
+  map.off();
+  map.remove();
+
+  for (var i = 0; i < state.searches.length; i++ ){
+  if (state.searches[i].search === searchText){
+    createMap(globLat, globLong, state.searches[i].geojson);
+    break;
+    }
+  }
+}
+
 // Ajax API Call
 var GetData = function(state, query){
   console.log(query);
-  // state.searches.push({search:'', items: []});
 
   var searchResults = {
     search : query,
@@ -127,7 +120,7 @@ var GetData = function(state, query){
 
   $.ajax({
   dataType: "json",
-  url: 'https://api.foursquare.com/v2/venues/search?ll='+lat + ',' + long + '&query=' + $('#queryfood').val() + '&client_id=' + client_id + '&client_secret=' + client_secret + '&v=20170301',
+  url: 'https://api.foursquare.com/v2/venues/search?ll='+globLat + ',' + globLong + '&query=' + $('#queryfood').val() + '&client_id=' + client_id + '&client_secret=' + client_secret + '&v=20170301',
   success: function(data){
     for (var i = 0;i < data.response.venues.length; i ++)
         {
@@ -135,22 +128,16 @@ var GetData = function(state, query){
           lat = data.response.venues[i].location.lat;
           long = data.response.venues[i].location.lng;
           title = data.response.venues[i].name;
-          // query = $('#queryfood').val();
-          // contact = data.response.venues[i].contact.phone;
           url = data.response.venues[i].url;
           addItem(state,item);
-          // state.geojson.features.push(mapLatLong(long,lat, title, url, query))
-
           searchResults.geojson.features.push(mapLatLong(long,lat,title,url,query))
         } //end of loop
-
-
-
         console.log(searchResults);
 
-        createMap(lat,long, searchResults.geojson);
+        //create the map
+        createMap(globLat,globLong, searchResults.geojson);
 
-
+        //populate the previous searches array
         state.searches.push(searchResults);
 
       } //end success
@@ -161,25 +148,15 @@ var GetData = function(state, query){
 }
 
 
-function getPreviousSearch(state, searchText) {
-
-  for (var i = 0; i < state.searches.length; i++ ){
-  if (state.searches[i].search === searchText){
-
-    createMap(lat, long, state.searches[i].geojson);
-    break;
-  }
-
-  }
-
-}
 
 
 //event handlers
 $(document).ready(function(){
+  getLocation();
+
   //create map to start
   L.mapbox.accessToken = token;
-    map = L.mapbox.map('map-one', 'mapbox.streets').setView([lat, long], 14);
+    map = L.mapbox.map('map-one', 'mapbox.streets').setView([globLat, globLong], 14);
 
 //click event for search
   $( "#submit" ).click(function(event) {
@@ -191,16 +168,20 @@ $(document).ready(function(){
       query = $('#queryfood').val();
       GetData(state, query);
       $('#queryfood').val('');
+
 //scroll down to map after submit
       $('html, body').animate({
           scrollTop: $("#map-section").offset().top
       }, 2000);
     });
 
-    $(document).on('click', '.searchLink', function(event){
-      event.preventDefault();
-      getPreviousSearch(state, $(this).text);
-      //
+
+    $(document).on('click', '.searchLink', function(){
+      var text = $(this).text();
+      getPreviousSearch(state, text);
     });
+
+
+
 
   });
